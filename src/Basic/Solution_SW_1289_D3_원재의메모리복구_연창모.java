1e389ffe536201de7a7c6cefd84cc644001c2450
연창모 <9oo9leplex@gmail.com>
2022-02-07 21:16:19 +0900

    array

---
+++ src/Basic/Solution_SW_1289_D3_원재의메모리복구_연창모.java
@@ -0,0 +1,32 @@
+package com.ssafy.class0203;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Solution_SW_1289_D3_원재의메모리복구_연창모 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		System.setIn(new FileInputStream("input.txt"));
+		Scanner sc = new Scanner(System.in);
+		
+		int tc = Integer.parseInt(sc.nextLine());
+		StringBuilder sb = new StringBuilder();
+		
+		for(int j=0;j<tc;j++) {
+			String line = sc.nextLine();
+			char[] arr = line.toCharArray();
+			
+			int result = 0;
+			if(arr[0]=='1') result++;
+			for(int i=0;i<line.length()-1;i++) {
+				if(arr[i] != arr[i+1]) result++;
+			}
+			sb.append("#").append(j+1).append(" ").append(result).append("\n");
+		}
+		System.out.println(sb);
+			
+	}
+
+}
