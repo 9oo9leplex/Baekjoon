37a1ff32ec48827f9809212f5139611836cea4c2
연창모 <9oo9leplex@gmail.com>
2022-02-14 20:19:31 +0900

    dynamicprogramming

--- src/DynamicProgramming/Baekjoon2670.java
+++ src/DynamicProgramming/Baekjoon2670.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Baekjoon2670 {
 
 	static int dp[],arr[];
 	static int k,n;
@@ -16,39 +16,25 @@ public class Main {
 //		StringTokenizer st = new StringTokenizer(br.readLine());
 //		n = Integer.parseInt(st.nextToken());
 //		k = Integer.parseInt(st.nextToken());
-//		StringBuilder sb = new StringBuilder();
-		
 		n = Integer.parseInt(br.readLine());
-		arr = new int[n];
-		dp = new int[n];
-		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i=0;i<n;i++)
-			arr[i] = Integer.parseInt(st.nextToken());
-		dp[0] = arr[0];
-		System.out.println(sol());
 		
-	}
-	
-	static int sol() {
-		int max = arr[0];
-		for(int i=1;i<n;i++) {
-			dp[i] = Math.max(arr[i], dp[i-1]+arr[i]);
+		arr = new int[n+1];
+		dp = new int[n+1];
+		for(int i=1;i<n+1;i++)
+			arr[i] = Integer.parseInt(st.nextToken());
+		
+		dp[1] = 1;
+		int max = dp[1];
+		for(int i=1;i<=n;i++) {
+			for(int j=0;j<i;j++) {
+				if(arr[i] > arr[j] && dp[i] <= dp[j])
+					dp[i] = dp[j] + 1;
+			}
 			max = Math.max(max, dp[i]);
 		}
-		return max;
+		
+		System.out.println(max);
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 }
