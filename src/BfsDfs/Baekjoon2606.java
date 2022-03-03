533d5abf34c31540a5ecd3891f65f81feb74b098
연창모 <9oo9leplex@gmail.com>
2022-03-03 22:06:07 +0900

    dfs

--- src/BfsDfs/Baekjoon2606.java
+++ src/BfsDfs/Baekjoon2606.java
@@ -3,11 +3,9 @@ package BfsDfs;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Baekjoon2606 {
 
 	static int n, k, result = 0,map[][];
 	static boolean visit[];
@@ -47,4 +45,5 @@ public class Main {
 			}
 		}
 	}
+
 }
