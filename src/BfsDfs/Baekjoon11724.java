479545bb17019fca9270f60b371093d707ed0b11
연창모 <9oo9leplex@gmail.com>
2022-03-06 22:43:35 +0900

    bfs

--- src/BfsDfs/Baekjoon11724.java
+++ src/BfsDfs/Baekjoon11724.java
@@ -3,14 +3,9 @@ package BfsDfs;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Baekjoon11724 {
 
 	static int map[][],n,m,result;
 	static boolean visit[];
@@ -53,4 +48,5 @@ public class Main {
 		
 		
 	}
+
 }
