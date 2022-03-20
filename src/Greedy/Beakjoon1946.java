4fe4f893fcbfab14244a3bd5dacd3df054ae6fcd
연창모 <9oo9leplex@gmail.com>
2022-03-20 23:25:14 +0900

    greedy

--- src/Greedy/Beakjoon1946.java
+++ src/Greedy/Beakjoon1946.java
@@ -6,8 +6,6 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-import Greedy.Main.Person;
-
 public class Beakjoon1946 {
 
 	static class Person implements Comparable<Person>{
