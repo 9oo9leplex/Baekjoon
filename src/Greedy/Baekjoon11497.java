0c3a351d850ad001ab540c2e523a82900464ca08
연창모 <9oo9leplex@gmail.com>
2022-03-21 22:59:05 +0900

    greedy

--- src/Greedy/Baekjoon11497.java
+++ src/Greedy/Baekjoon11497.java
@@ -3,16 +3,11 @@ package Greedy;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.PriorityQueue;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main {
-	
+public class Baekjoon11497 {
+
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
