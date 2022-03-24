3d25572bc7762fae625e251fd55e6794ac2d0302
연창모 <9oo9leplex@gmail.com>
2022-03-24 23:18:07 +0900

    impl

--- src/Implementation/Baekjoon2615.java
+++ src/Implementation/Baekjoon2615.java
@@ -4,11 +4,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
-import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Baekjoon2615 {
 
 	static int[] dr = {-1,-1,-1,0,1,1,1,0};
 	static int[] dc = {-1,0,1,1,1,0,-1,-1};
@@ -74,6 +73,4 @@ public class Main {
 	private static boolean inArea(int nr, int nc) {
 		return nr >= 0 && nr < 19 && nc >= 0 && nc < 19;
 	}
-
-	
 }
