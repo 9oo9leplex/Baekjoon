96a273051190d97b797396d7f5b2e2d96182d121
연창모 <9oo9leplex@gmail.com>
2022-03-25 23:01:27 +0900

    impl

--- src/Implementation/Main.java
+++ src/Implementation/Main.java
@@ -3,9 +3,9 @@ package Implementation;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -16,63 +16,40 @@ public class Main {
 	public static void main(String[] args) throws IOException{
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
 		
-		Queue<int[]> q = new LinkedList<>();
-		int[][] map = new int[19][19];
-		int tmp;
+		int n = Integer.parseInt(br.readLine());
+		int p = Integer.parseInt(br.readLine());
 		
-		for(int i=0;i<19;i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j=0;j<19;j++) {
-				tmp = Integer.parseInt(st.nextToken());
-				map[i][j] = tmp;
-				if(tmp != 0) q.add(new int[] {i,j});
-			}
-		}
+		int[] chuchun = new int[101];
+		LinkedList<Integer> q = new LinkedList<>();
 		
-		int result,nr,nc;
-		StringBuilder sb = new StringBuilder();
 		
-		while(!q.isEmpty()) {
-			int[] a = q.poll();
-			tmp = map[a[0]][a[1]];
+		int num,min,tmp;
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=1;i<=p;i++) {
+			num = Integer.parseInt(st.nextToken());
+			if(!q.contains(num)) q.add(num);
+			chuchun[num]++;
 			
-			for(int i=0;i<8;i++) {
-				result = 0;
-				nr = a[0] + dr[i] * 5;
-				nc = a[1] + dc[i] * 5;
-				if(inArea(nr, nc) && tmp == map[nr][nc]) continue;
-				
-				nr = a[0] - dr[i];
-				nc = a[1] - dc[i];
-				if(inArea(nr, nc) && tmp == map[nr][nc]) continue;
-				
-				for(int j=1;j<=4;j++) {
-					nr = a[0] + dr[i]*j;
-					nc = a[1] + dc[i]*j;
-					if(inArea(nr, nc) && tmp == map[nr][nc]) result++;
-					else break;
+			if(q.size() > n ) {
+				min = chuchun[q.get(0)];
+				tmp=0;
+				for(int j=1;j<n-1;j++) {
+					if(chuchun[q.get(j)] < min) {
+						min = q.get(j);
+						tmp = j;
+					}
 				}
-				
-				if(result == 4) {
-					sb.append(tmp).append("\n");
-					if(i==0 || i==6 || i==7)
-						sb.append(a[0]+dr[i]*4+1).append(" ").append(a[1]+dc[i]*4+1);
-					else
-						sb.append(a[0]+1).append(" ").append(a[1]+1);
-					
-					
-					System.out.println(sb);
-					return;
-				}
+				chuchun[q.remove(tmp)] = 0;
 			}
+//			System.out.println(Arrays.toString(chuchun));
 		}
-		System.out.println(0);
-	}
-
-	private static boolean inArea(int nr, int nc) {
-		return nr >= 0 && nr < 19 && nc >= 0 && nc < 19;
+		Collections.sort(q);
+		StringBuilder sb = new StringBuilder();
+		for(int a : q)
+			sb.append(a).append(" ");
+		System.out.println(sb);
 	}
 
 	
