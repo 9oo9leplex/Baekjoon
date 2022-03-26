9ba24577ca08a26917c8a2265b9635f008e8c6cf
연창모 <9oo9leplex@gmail.com>
2022-03-26 22:50:02 +0900

    impl

--- src/Implementation/Main.java
+++ src/Implementation/Main.java
@@ -10,47 +10,147 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	static int[] dr = {-1,-1,-1,0,1,1,1,0};
-	static int[] dc = {-1,0,1,1,1,0,-1,-1};
+	
+	
 	
 	public static void main(String[] args) throws IOException{
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int n = Integer.parseInt(br.readLine());
-		int p = Integer.parseInt(br.readLine());
-		
-		int[] chuchun = new int[101];
-		LinkedList<Integer> q = new LinkedList<>();
-		
-		
-		int num,min,tmp;
-		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i=1;i<=p;i++) {
-			num = Integer.parseInt(st.nextToken());
-			if(!q.contains(num)) q.add(num);
-			chuchun[num]++;
-			
-			if(q.size() > n ) {
-				min = chuchun[q.get(0)];
-				tmp=0;
-				for(int j=1;j<n-1;j++) {
-					if(chuchun[q.get(j)] < min) {
-						min = q.get(j);
-						tmp = j;
-					}
-				}
-				chuchun[q.remove(tmp)] = 0;
+		int r = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(st.nextToken());
+		
+		int map[] = new int[257];
+		LinkedList<Integer> q = new LinkedList<>();
+		int tmp,sum,cnt,later,tmpN,time = 0,height = 0,result = Integer.MAX_VALUE;
+		for(int i=0;i<r;i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0;j<c;j++) {
+				tmp = Integer.parseInt(st.nextToken());
+				map[tmp]++;
+				if(!q.contains(tmp)) q.add(tmp);
 			}
-//			System.out.println(Arrays.toString(chuchun));
 		}
+		
 		Collections.sort(q);
-		StringBuilder sb = new StringBuilder();
-		for(int a : q)
-			sb.append(a).append(" ");
-		System.out.println(sb);
+		height = -1;
+		
+		for(int i=0;i<q.size();i++) {
+			tmp = q.get(i);
+			tmpN = n;
+			sum = 0;
+			time = 0;
+			cnt = 0;
+			
+			for(int j=0;j<q.size();j++) {
+				if(i==j) continue;
+				int a = q.get(j);
+				int diff = a - tmp;
+				
+				if(diff > 0) {
+//					System.out.println("diff: "+diff+ ", tmp: "+tmp+", now: "+a+", size: "+map[a]);
+					time += (diff*map[a]*2);
+					tmpN += (diff*map[a]);
+				} else if(diff < 0) {
+//					System.out.println("diff: "+diff+ ", tmp: "+tmp+", now: "+a+", size: "+map[a]);
+					time += Math.abs(diff)*map[a];
+					tmpN -= Math.abs(diff)*map[a];
+				}
+				
+//				System.out.println("time: "+time+", block: "+tmpN);
+				
+			}
+			if(tmpN >= 0) {
+				if(result >= time) {// || (result == time && height < tmp)) {
+					result = time;
+					height = tmp;
+				}
+			}
+//			later = map[tmp];
+//			System.out.println("기준: "+tmp);
+			// 현재 기준보다 큰 값 들을 깎는다
+			/*
+			for(int j=i+1;j<q.size();j++) {
+				time += map[q.get(j)] * (q.get(j) - tmp) * 2;
+				tmpN += map[q.get(j)] * (q.get(j) - tmp);
+//				later += map[q.get(j)];
+			}
+			// 현재 기준보다 작은 값들에 쌓는다
+			for(int j=0;j<i;j++) {
+//				System.out.println("방문 높이: "+q.get(j)+", 방문높이 갯수: "+map[q.get(j)]);
+//				System.out.println("추가된 블럭: "+ map[q.get(j)] * (tmp - q.get(j)));
+//				sum += map[q.get(j)] * (tmp - q.get(j));
+				time += map[q.get(j)] * (tmp - q.get(j)) * 2;
+				tmpN -= map[q.get(j)] * (tmp - q.get(j));
+			}
+			
+			System.out.println("block: "+tmpN+", time: "+time+", result: "+result);
+			
+			if(tmpN >= 0) {
+				if(time <= result) {
+					height = tmp;
+					result = time;
+				}
+				else if(height <= tmp) {
+					height = tmp;
+					result = time;
+				}
+			}
+			*/
+//			System.out.println(" 남은 블럭: "+tmpN+", 쌓아야할 갯수: "+sum);
+			/*
+			if(sum > tmpN) {
+				while(true) {
+					if(sum <= tmpN) break;
+					tmpN += (later);
+					time += (later) * 2;
+					cnt++;
+//					System.out.println("기준 높이: "+tmp+", 블럭: "+tmpN);
+				}
+				tmp -= cnt;
+			}
+			
+			time += sum;
+			*/
+//			System.out.println("기존 시간:현재 시간 "+result+" : "+time+", 기존 높이:현재 높이 "+height+" : "+tmp);
+//			System.out.println("조건: "+ (result > time && tmp > height));
+			// 시간이 적게 걸리고 높이가 더 높다면 변경
+			/*
+			if(result >= time) {
+				if(result == time) {
+//					System.out.println("1");
+					if(tmp > height) {
+						result = time;
+						height = tmp;
+					}
+				} else {
+//					System.out.println("2");
+					result = time;
+					height = tmp;
+				}
+			}
+			*/
+		}
+		
+		
+		System.out.println(result+" "+height);
 	}
 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
