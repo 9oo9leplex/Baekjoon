e945a500f66705e70474322b0274630406897ca0
연창모 <9oo9leplex@gmail.com>
2022-03-27 19:42:57 +0900

    impl, Baekjoon17609

--- src/Implementation/Baekjoon17609.java
+++ src/Implementation/Baekjoon17609.java
@@ -3,19 +3,10 @@ package Implementation;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.StringTokenizer;
 
-public class Main {
+public class Baekjoon17609 {
 
-	
-	static int dx[] = {0,1,0,-1};
-	static int dy[] = {1,0,-1,0};
-	static int maxX, minX, maxY, minY;
-	
-	public static void main(String[] args) throws IOException{
+public static void main(String[] args) throws IOException{
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	
--- src/Implementation/Baekjoon8911.java
+++ src/Implementation/Baekjoon8911.java
@@ -4,8 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-import Implementation.Main.Turtle;
-
 public class Baekjoon8911 {
 
 	static class Turtle{
