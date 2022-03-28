1b529e99194619cfcb54c4cdbd3ca270131e9641
연창모 <9oo9leplex@gmail.com>
2022-03-28 23:57:11 +0900

    impl, Baekjoon17829

--- src/Implementation/Main.java
+++ src/Implementation/Main.java
@@ -3,106 +3,65 @@ package Implementation;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-	
+
 	static int dr[] = {-1,0,1,0};
 	static int dc[] = {0,1,0,-1};
-	static int map[][], n,s;
-	static List<Virus> q;
-	
-	static class Virus implements Comparable<Virus>{
-		int x,y,priority;
-
-		public Virus(int x, int y, int priority) {
-			super();
-			this.x = x;
-			this.y = y;
-			this.priority = priority;
-		}
-
-		@Override
-		public int compareTo(Virus o) {
-			return this.priority - o.priority;
-		}
-	}
+	static int map[][], result[][],n,s;
 	
 	public static void main(String[] args) throws IOException{
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));	
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		n = Integer.parseInt(st.nextToken());
-		int k = Integer.parseInt(st.nextToken());
+		n = Integer.parseInt(br.readLine());
+		StringTokenizer st;
 		
 		map = new int[n][n];
-		q = new LinkedList<>();
-		int tmp;
-		
+		result = new int[n][n];
 		for(int i=0;i<n;i++) {
 			st = new StringTokenizer(br.readLine());
 			for(int j=0;j<n;j++) {
-				tmp = Integer.parseInt(st.nextToken());
-				map[i][j] = tmp;
-				if(tmp != 0) q.add(new Virus(i,j,tmp));
+				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
+		int len = n;
+		List<Integer> list = new ArrayList<>();
 		
-		st = new StringTokenizer(br.readLine());
-		s = Integer.parseInt(st.nextToken());
-		int x = Integer.parseInt(st.nextToken()) - 1;
-		int y = Integer.parseInt(st.nextToken()) - 1;
+		int row = 0, col = 0;
 		
-		bfs();
-		
-//		for(int i=0;i<n;i++) {
-//			System.out.println(Arrays.toString(map[i]));
-//		}
-		
-		System.out.println(map[x][y]);
-	}
-
-	private static void bfs() {
-
-		int day = 0;
-		
-		while(!q.isEmpty()) {
-			if(s == day) break;
+		while(true) {
+			if(len < 1) break;
 			
-			int size = q.size();
-			int cnt = 0;
-			
-			Collections.sort(q);
-			
-			while(size > cnt) {
-				Virus v = q.remove(0);
-				int value = v.priority;
-//				System.out.println(value);
-				for(int i=0;i<4;i++) {
-					int nr = v.x + dr[i];
-					int nc = v.y + dc[i];
-					
-					if(inArea(nr,nc) && map[nr][nc] == 0) {
-						map[nr][nc] = value;
-						q.add(new Virus(nr,nc,value));
-					}
+			for(int r=0;r<len;r+=2) {
+				for(int c=0;c<len;c+=2) {
+					list.add(map[r][c]);
+					list.add(map[r][c+1]);
+					list.add(map[r+1][c]);
+					list.add(map[r+1][c+1]);
+					Collections.sort(list);
+					map[row][col] = list.get(2);
+					col++;
+					list.clear();
 				}
-				cnt++;
+				row++;
+				col = 0;
 			}
-			day++;
-//			System.out.println("s: "+s+", day: "+day+ (s > day));
+			row = 0;
+			len /= 2;
 		}
+		
+		System.out.println(map[0][0]);
+	
+		
+		
 	}
 
 	private static boolean inArea(int nr, int nc) {
 		return nr >= 0 && nr < n && nc >= 0 && nc < n;
 	}
-	
-	
-	
-	
 }
