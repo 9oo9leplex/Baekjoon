b7bfc33aec37d940e4440ffab1b2aeadfbbe7c7d
연창모 <9oo9leplex@gmail.com>
2022-04-02 18:29:00 +0900

    impl

--- src/IM/Main.java
+++ src/IM/Main.java
@@ -3,58 +3,85 @@ package IM;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static int n,map[][],max;
+	static int n,k,map[][],max;
+	static Queue<int[]> apple;
+	static class Snake{
+		int r,c,dir;
+
+		public Snake(int r, int c, int dir) {
+			super();
+			this.r = r;
+			this.c = c;
+			this.dir = dir;
+		}
+	}
+	
+	static int[] dr = {-1,0,1,0};
+	static int[] dc = {0,-1,0,1};
 	
 	public static void main(String[] args) throws IOException{
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		n = Integer.parseInt(br.readLine());
+		k = Integer.parseInt(br.readLine());
+			
+		map = new int[n][n];
+		apple = new LinkedList<>();
 		
 		StringTokenizer st;
-		map = new int[n][6];
-		for(int i=0;i<n;i++) {
+		for(int i=0;i<k;i++) {
 			st = new StringTokenizer(br.readLine());
-			for(int j=0;j<6;j++) {
-				if(j==3)
-					map[i][4] = Integer.parseInt(st.nextToken());
-				else if(j==4)
-					map[i][3] = Integer.parseInt(st.nextToken());
-				else
-					map[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
 			
-		for(int i=0;i<6;i++)
-			dfs(map[0][i],0,0);
-		System.out.println(max);
-		
-	}
-
-	private static void dfs(int down, int index, int sum) {
-		if(index == n) {
-			max = Math.max(max, sum);
-			return;
+			int r = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			apple.add(new int[] {r,c});		
 		}
-		if(sum + (n-index)*6 <= max) return;
 		
-		int up=0;
-		for(int i=0;i<6;i++) {
-			if(map[index][i] == down) {
-				up = map[index][5-i];
-				break;
+		int t = Integer.parseInt(br.readLine());
+		
+		Queue<Integer> time = new LinkedList<>();
+		Queue<Character> dir = new LinkedList<>();
+		
+		for(int i=0;i<t;i++) {
+			st = new StringTokenizer(br.readLine());
+			time.add(Integer.parseInt(st.nextToken()));
+			dir.add(st.nextToken().charAt(0));
+		}
+		
+		t = 0;
+		char d;
+		Snake snake = new Snake(0, 0, 3);
+		while(true) {
+			if(!inArea(snake.r,snake.c)) break;
+//			System.out.println(t+", r: "+snake.r+", c: "+snake.c);
+			// 자기몸에 부딪치면 사망
+			if(!time.isEmpty() && t == time.peek()) {
+				time.poll();
+				switch(dir.poll()) {
+				case 'L':
+					snake.dir = (snake.dir + 1)%4;
+					break;
+				case 'D':
+					snake.dir = snake.dir - 1 < 0 ? 3 : snake.dir - 1;
+					break;
+				}
 			}
+			
+			snake.r += dr[snake.dir];
+			snake.c += dc[snake.dir];
+			
+			t++;
 		}
-		
-		int maxside = 0;
-		for(int i=0;i<6;i++) {
-			if(map[index][i] == down || map[index][i] == up) continue;
-			maxside = Math.max(maxside, map[index][i]);
-		}
-		dfs(up,index+1,sum+maxside);
+		System.out.println(t);
 	}
 
+	private static boolean inArea(int r, int c) {
+		return r >= 0 && c >= 0 && r < n && c < n;
+	}
 }
