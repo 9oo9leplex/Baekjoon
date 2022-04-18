e6e5df6e70df38eb8686f8226a12604167e3951d
연창모 <9oo9leplex@gmail.com>
2022-04-18 23:55:58 +0900

    Simulation,Baekjoon20055

--- src/Simulation/Main.java
+++ src/Simulation/Main.java
@@ -3,102 +3,90 @@ package Simulation;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static class Wheel {
-		
-		int start;
+	static int N,K,arr[];
 
-		public Wheel(int start) {
-			super();
-			this.start = start;
-		}
-		
-	}
-	
-	static int w[][];
-	static Wheel[] wheels;
-	static Queue<Integer> leftRotate = new LinkedList<>();
-	static Queue<Integer> rightRotate = new LinkedList<>();
 	
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-	
-		wheels = new Wheel[4];
-		w = new int[4][8];
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		for(int i=0;i<4;i++) {
-			char[] tmp = br.readLine().toCharArray();
-			wheels[i] = new Wheel(0);
-			for(int j=0;j<8;j++) {
-				w[i][j] = tmp[j] - '0';
-			}
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		
+		arr = new int[2*N];
+		Deque<Integer> upSideBelt = new LinkedList<>();
+		Deque<Integer> downSideBelt = new LinkedList<>();
+		Queue<Integer> robot = new LinkedList<>();
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0;i<2*N;i++) 
+			arr[i] = Integer.parseInt(st.nextToken());
+		
+		for(int i=0;i<N;i++) {
+			upSideBelt.add(i);
+			downSideBelt.add(2*N-1-i);
 		}
 		
-		int n = Integer.parseInt(br.readLine());
-		int sum = 0;
-		for(int i=0;i<n;i++) {
-			st = new StringTokenizer(br.readLine());
-			int target = Integer.parseInt(st.nextToken())-1;
-			int dir = Integer.parseInt(st.nextToken());
+		int first, len, bot, next, cnt=0;
+		
+		while(K > 0) {
 			
-			if(dir > 0) rightRotate.add(target);
-			else leftRotate.add(target);
+			cnt++;
 			
-			for(int j=target;j<3;j++) {
-//				System.out.println("[j,right]: ["+j+","+(wheels[j].start+2)%8+"], [j+1,left]: ["+(j+1)+","+(wheels[j+1].start+6)%8+"]");
-				if(w[j][(wheels[j].start+2)%8] != w[j+1][(wheels[j+1].start+6)%8]) {
-					if((j+1+target) % 2 == 0) {
-						if(dir > 0) rightRotate.add(j+1);
-						else leftRotate.add(j+1);
-					}
-					else {
-						if(dir > 0) leftRotate.add(j+1);
-						else rightRotate.add(j+1);
-					}
-				} else break;
+			// 벨트 이동
+			downSideBelt.addLast(upSideBelt.pollLast());
+			upSideBelt.addFirst(downSideBelt.pollFirst());
+			
+			System.out.println(upSideBelt);
+			System.out.println(downSideBelt);
+			System.out.println("현재 작업: "+robot);
+			// 로봇 이동
+			len = robot.size();
+			for(int i=0;i<len;i++) {
+				bot = robot.poll();
+				if(upSideBelt.peekLast() == bot) continue;
+				next = (bot+1) %( 2 * N);
+				if(arr[next] > 0) {
+					arr[next]--;
+					if(isZero(next)) K--;
+					robot.add(next);
+				} else robot.add(bot);
 			}
 			
-			for(int j=target;j>0;j--) {
-//				System.out.println("[j,left]: ["+j+","+(wheels[j].start+6)%8+"], [j-1,right]: ["+(j-1)+","+(wheels[j-1].start+2)%8+"]");
-				if(w[j][(wheels[j].start+6)%8] != w[j-1][(wheels[j-1].start+2)%8]) {
-					if((j-1+target) % 2 == 0) {
-						if(dir > 0) rightRotate.add(j-1);
-						else leftRotate.add(j-1);
-					}
-					else {
-						if(dir > 0) leftRotate.add(j-1);
-						else rightRotate.add(j-1);
-					}
-				} else break;
+			System.out.println("로봇 이동 후");
+			System.out.println(Arrays.toString(arr));
+			
+			
+			// 로봇 올리기
+			first = upSideBelt.peekFirst();
+			if(arr[first] > 0) {
+				robot.add(first);
+				arr[first]--;
+				if(isZero(first)) K--;
 			}
 			
-//			System.out.println("left: "+leftRotate);
-//			System.out.println("right: "+rightRotate);
-			
-			while(!leftRotate.isEmpty()) {
-				int tmp = leftRotate.poll();
-				wheels[tmp].start = (wheels[tmp].start + 1) % 8;
-			}
-			
-			while(!rightRotate.isEmpty()) {
-				int tmp = rightRotate.poll();
-				wheels[tmp].start = (wheels[tmp].start - 1) < 0 ? 7 : wheels[tmp].start - 1;
-			}
-			
-			
-			
+			System.out.println("로봇 올린 후");
+			System.out.println(Arrays.toString(arr));
 		}
-		for(int j=0;j<4;j++) {
-			if(w[j][wheels[j].start] == 1) sum += Math.pow(2,j);
-		}
-		System.out.println(sum);
+		
+		System.out.println(cnt);
+		
+		
+	}
+
+
+	private static boolean isZero(int index) {
+		return arr[index] == 0;
 	}
 
 	
