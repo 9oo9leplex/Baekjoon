77d8ae2a595f87f1751ffc85efc48e503073743b
연창모 <9oo9leplex@gmail.com>
2022-04-29 23:44:58 +0900

    BFS,Baekjoon18513

--- src/BfsDfs/Main.java
+++ src/BfsDfs/Main.java
@@ -8,24 +8,29 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int N,M,T,time,point,map[][];
-	static boolean[][] visit;
-	static boolean getSword, isFail;
-	
-	static class Destination{
-		int r,c;
 
-		public Destination(int r, int c) {
-			super();
-			this.r = r;
-			this.c = c;
-		}
-		
-	}
+	static int N,K;
+	static long sum;
+	static Queue<Integer> q;
 	
-	static int dr[] = {-1,0,1,0};
-	static int dc[] = {0,1,0,-1};
+	static int[] d = {-1,1};
+//	static House[] wells;
+	static Queue<House> houses;
+	static class House {
+		int index,dir,distance;
+
+		public House(int index, int dir, int distance) {
+			super();
+			this.index = index;
+			this.dir = dir;
+			this.distance = distance;
+		}
+
+		@Override
+		public String toString() {
+			return "House [index=" + index + ", dir=" + dir + ", distance=" + distance + "]";
+		}		
+	}
 	
 	public static void main(String[] args) throws IOException {
 
@@ -33,108 +38,106 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		T = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
 		
-		map = new int[N][M];
-		visit = new boolean[N][M];
-		// 칼 ~ 도착지점 저장 변수
-		int distance = 0;
+//		map = new int[200000001];
+//		wells = new House[N];
+		q = new LinkedList<>();
+		houses = new LinkedList<>();
 		
+		st = new StringTokenizer(br.readLine());
+		
+		int well; 
 		for(int i=0;i<N;i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j=0;j<M;j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-				if(map[i][j] == 2) {
-					// 도착 지점 ~ 칼 거리
-					distance = ((N-1) - i) + ((M-1) - j);
-				}
-			}
+			well = Integer.parseInt(st.nextToken());
+			houses.add(new House(well, 0,0));
+//			map[well] = 2;
+//			wells[i] = new House(well, 0);
+			q.add(well);
 		}
 		
-		// 칼을 얻었는지 판별 변수
-		getSword = false;
-		// 도착 못하는 지 저장 변수
-		isFail = false;
-
-		bfs(0,0);
-
-		// 도착하든 못하든 칼을 얻었는지
-		if(getSword) {
-			// 도착 못했다면 time 변수가 초기 값 0임
-			// 도착 못하고 && 칼~도착 거리 + 출발~칼 거리 > 시간 제한
-			if(time == 0 && distance + point > T) System.out.println("Fail");
-			// 도착 했으면 ? Math.min(걸린시간, 출발 ~ 칼 ~ 도착 거리) : 칼~도착 + 출발~칼 : 도착 못했다면 칼~도착 + 출발~칼 
-			else System.out.println(time != 0 ? Math.min(time, distance + point) : distance + point);
-		}
-		// 칼 못 얻었으면
-		else {
-			// 도착 못했다면
-			if(isFail) System.out.println("Fail");
-			// 도착했다면
-			else System.out.println(time);
-		}
-		
-	}
-	
-	private static void bfs(int r, int c) {
-
-		Queue<Destination> q = new LinkedList<>();
-		q.add(new Destination(r, c));
-		visit[r][c] = true;
-		
-		int cnt = -1;
-		// 큐 사이즈, 1 사이클
-		int size,loop;
-		
-		while(!q.isEmpty()) {
-			
-			size = q.size();
-			loop = 0;
-			cnt++;
-			
-			// 1사이클 끝나고 시간 제한 걸리는 지 체크
-			if(cnt > T) {
-				isFail = true;
-				break;
-			}
-			// 1 사이클
-			while(size > loop) {
-			
-				Destination d = q.poll();
-				loop++;
-				// 도착했다면 time에 시간 저장
-				if(d.r == N-1 && d.c == M-1) {
-					time = cnt;
-					return;
-				}
-				// 4방 탐색
-				for(int i=0;i<4;i++) {
-					int nr = d.r + dr[i];
-					int nc = d.c + dc[i];
-					// 범위[N,M] 안이고, 방문한 적 없고, 벽이 아니면
-					if(inArea(nr,nc) && !visit[nr][nc] && map[nr][nc] != 1) {
-						// 방문 처리
-						visit[nr][nc] = true;
-						// 칼이면
-						if(map[nr][nc] == 2) {
-							// 시작 ~ 칼 거리 저장
-							point = cnt+1;
-							// 칼 획득 
-							getSword = true;
-						}
-						// 다음 위치 저장
-						q.add(new Destination(nr, nc));
-					}
-				}
-			}
-		}
-		
-		// 큐가 없어질 동안 도달 못했다면 Fail
-		isFail = true;
+		bfs();
+		System.out.println(sum);
 	}
 
-	private static boolean inArea(int nr, int nc) {
-		return nr >= 0 && nr < N && nc >= 0 && nc < M;
+	private static void bfs() {
+
+		int len = houses.size(), cnt, next, num = 0;
+		House h;
+		
+		for(int i=0;i<len;i++) {
+			h = houses.poll();
+			if(inArea(h.index-1) && !q.contains(h.index-1)) {
+				houses.add(new House(h.index-1, 0,1));
+				q.add(h.index-1);
+				sum += 1;
+				num++;
+			}
+			
+			if(K == num) return;
+			
+			if(inArea(h.index+1) && !q.contains(h.index+1)) {
+				houses.add(new House(h.index+1, 1,1));
+				q.add(h.index+1);
+				sum += 1;
+				num++;
+			}
+			
+			if(K == num) return;
+		}
+		
+//		System.out.println(houses);
+		
+		while(!houses.isEmpty()) {
+			
+			h = houses.poll();
+//				System.out.println("now: "+h+", cnt: "+num);
+			next = h.index+d[h.dir];
+			if(inArea(next)) {
+				
+				houses.add(new House(next, h.dir,h.distance+1));
+			
+				if(!q.contains(next)) {
+					q.add(next);
+					num++;
+	//					System.out.println("[num,sum]: ["+num+","+sum+"]");
+					sum += h.distance+1;
+				}
+			}
+			
+			if(K == num) return;
+			
+
+			
+		}
+		
+		
+		
+	}
+
+	private static boolean inArea(int next) {
+		return next >= -100000000 && next < 100000001;
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/Simulation/Main.java
+++ src/Simulation/Main.java
@@ -3,139 +3,16 @@ package Simulation;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
 
 public class Main {
-	/*
-	static int total = 720;
 	
-	static class TimeTable implements Comparable<TimeTable>{
-		int start,end,index;
-
-		public TimeTable(int start, int end) {
-			super();
-			this.start = start;
-			this.end = end;
-		}
-
-		@Override
-		public int compareTo(TimeTable o) {
-			if(this.start == o.start) return this.start - o.start;
-			return this.start - o.start;
-		}
-
-		@Override
-		public String toString() {
-			return "TimeTable [start=" + start + ", end=" + end + "]";
-		}
+	public static void main(String[] args) throws IOException{
 		
-	}
-	
-	static List<TimeTable> time,sit;
-	static boolean[] seat;
-	static int n,t,p;
-	*/
-	public static void main(String[] args) throws IOException {
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new  StringTokenizer(br.readLine());
 		
-		int a = Integer.parseInt(st.nextToken());
-		int b = Integer.parseInt(st.nextToken());
-		
-		StringBuilder sb = new StringBuilder();
-		sb.append(a+b).append("\n");
-		sb.append(a-b).append("\n");
-		sb.append(a*b).append("\n");
-		sb.append(a/b).append("\n");
-		sb.append(a%b).append("\n");
-		System.out.println(sb);
-		/*
-		n = Integer.parseInt(st.nextToken());
-		t = Integer.parseInt(st.nextToken());
-		p = Integer.parseInt(st.nextToken())-1;
-		
-		time = new ArrayList<>();
-		
-		int start,end;
-		for(int i=0;i<t;i++) {
-			st = new StringTokenizer(br.readLine());
-			start = Integer.parseInt(st.nextToken());
-			end = Integer.parseInt(st.nextToken());
-			
-			time.add(new TimeTable(start,end));
-		}
-
-		Collections.sort(time);
-		
-		seat = new boolean[n];
-		sit = new ArrayList<>();
-		
-		while(!time.isEmpty()) {
-			
-			TimeTable now = time.remove(0);
-			if(sit.isEmpty()) {
-				seatFirst(now);
-			} else {
-				// 현재 시작 시간, 이전에 있던 놈들 끝나는 시간 비교해서 내보내기
-				for(TimeTable tt : sit) {
-					if(now.start >= tt.end) {
-						sit.remove(tt);
-						// 내보낸 놈들 자리 복구
-						seat[tt.index] = false;
-					}
-				}
-				if(sit.isEmpty()) {
-					seatFirst(now);
-					continue;
-				} 
-				
-				// 남아 있는 자리 중에서 가장 먼 자리 탐색
-				findSeat();
-				// 선호하는 자리인지 비교
-			}
-			
-		}
-		
-		
+		System.out.println(Integer.parseInt(br.readLine()) + Integer.parseInt(br.readLine()));
 	}
-
-	private static void findSeat() {
-
-		if(sit.size() == 1) {
-			int d = Math.max(sit.get(0).index,n-sit.get(0).index);
-		} else {
-			
-		}
-		
-	}
-
-	private static void seatFirst(TimeTable now) {
-		seat[0] = true;
-		now.index = 0;
-		sit.add(now);
-		
-		if(p == 0) {
-			// end - start
-			cal(now);
-		}
-	}
-
-	private static void cal(TimeTable now) {
-
-		int sH = now.start / 100;
-		int sM = now.start % 100;
-		int eH = now.end / 100;
-		int eM = now.end % 100;
-		
-		
-		total -= ( eH * 60 + eM ) - (sH * 60 + sM);
-		*/
-	}
+	
 }
 
 
