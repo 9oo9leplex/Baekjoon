8a06faacb36329cc38883f022ab4136ad368fe46
연창모 <9oo9leplex@gmail.com>
2022-05-13 23:41:14 +0900

    BFS,Baekjoon2665

---
+++ src/BfsDfs/Baekjoon2665.java
@@ -0,0 +1,75 @@
+package BfsDfs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Baekjoon2665 {
+
+	static int N, map[][], dp[][];
+	static class Node{
+		int r,c;
+
+		public Node(int r, int c) {
+			super();
+			this.r = r;
+			this.c = c;
+		}
+		
+	}
+	static int[] dr = {-1,0,1,0};
+	static int[] dc = {0,1,0,-1};
+	
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		map = new int[N][N];
+		dp = new int[N][N];
+		char[] tmp;
+		for(int i=0;i<N;i++) {
+			tmp = br.readLine().toCharArray();
+			for(int j=0;j<N;j++) {
+				map[i][j] = tmp[j] - '0';
+				dp[i][j] = Integer.MAX_VALUE;
+			}
+		}
+		
+		bfs();
+		System.out.println(dp[N-1][N-1]);
+	}
+
+	private static void bfs() {
+
+		Queue<Node> q = new LinkedList<>();
+		q.add(new Node(0,0));
+		dp[0][0] = 0;
+		
+		Node node;
+		int nr,nc;
+		
+		while(!q.isEmpty()) {
+			node = q.poll();
+			
+			for(int i=0;i<4;i++) {
+				nr = node.r + dr[i];
+				nc = node.c + dc[i];
+				
+				if(inArea(nr,nc) && dp[nr][nc] > dp[node.r][node.c]) {
+					if(map[nr][nc] == 1) dp[nr][nc] = dp[node.r][node.c];
+					else dp[nr][nc] = dp[node.r][node.c] + 1;
+					q.add(new Node(nr,nc));
+				}
+			}
+			
+		}
+		
+	}
+
+	private static boolean inArea(int nr, int nc) {
+		return nr >= 0 && nr < N && nc >= 0 && nc < N;
+	}
+}
--- src/BfsDfs/Main.java
+++ src/BfsDfs/Main.java
@@ -3,151 +3,124 @@ package BfsDfs;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-// 2234
+// 24513, 틀렸음
 public class Main {
 
-	static int N, M, map[][], dp[][];
-	static class Node{
-		int r,c,d;
+	static int N, M, zero, map[][];
 
-		public Node(int r, int c, int d) {
+	static class Node {
+		int r, c, num;
+
+		public Node(int r, int c, int num) {
 			super();
 			this.r = r;
 			this.c = c;
-			this.d = d;
-		}		
+			this.num = num;
+		}
+
+		@Override
+		public String toString() {
+			return "Node [r=" + r + ", c=" + c + ", t=" + num + "]";
+		}
+
 	}
-	
-	static int[] dr = {0,-1,0,1};
-	static int[] dc = {-1,0,1,0};
+
+	static int[] dr = { 0, -1, 0, 1 };
+	static int[] dc = { -1, 0, 1, 0 };
 	static boolean[][] visit;
-	
+	static Queue<Node> virus;
+
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		M = Integer.parseInt(st.nextToken());
+
 		N = Integer.parseInt(st.nextToken());
-		
+		M = Integer.parseInt(st.nextToken());
+
 		map = new int[N][M];
+		virus = new LinkedList<>();
 		visit = new boolean[N][M];
 		
 		for(int i=0;i<N;i++) {
 			st = new StringTokenizer(br.readLine());
 			for(int j=0;j<M;j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
+				if(map[i][j] == -1) visit[i][j] = true;
+				else if(map[i][j] == 0) zero++;
+				else virus.add(new Node(i,j,map[i][j]));
 			}
 		}
 		
 		bfs();
+		countNum();
+	}
+
+	private static void countNum() {
+
+		int one = 0,two = 0,three = 0, val;
+		
+		for(int i=0;i<N;i++) {
+			for(int j=0;j<M;j++) {
+				val = map[i][j];
+				if(val == -1) continue;
+				else if(val == 1) one++;
+				else if(val == 2) two++;
+				else three++;
+			}
+		}
+		System.out.println(one+" "+two+" "+three);
 	}
 
 	private static void bfs() {
 
-		Queue<Node> q = new LinkedList<>();
+		
+		int nr,nc,size;
 		Node node;
-		int nr,nc,size,max = Integer.MIN_VALUE,room=0;
 		
-		// map과 똑같은 크기의 배열을 하나 더 만들고 i입력
-		int[][] test = new int[N][M];
-		// i 갯수 카운팅해서 따로 가지고 있고
-		List<Integer> dp = new ArrayList<>();
-		// 배열 사방 탐색해서 max 값 초기화 visit 써서 이미 더한 곳 제외
-		
-		int index = 0;
-		
-		for(int i=0;i<N;i++) {
-			for(int j=0;j<M;j++) {
+		while(!virus.isEmpty()) {
+			size = virus.size();
+			for(int i=0;i<size;i++) {
+				node = virus.poll();
+				visit[node.r][node.c] = true;
+				if(map[node.r][node.c] == 3) continue;
 				
-				if(visit[i][j]) continue;
-				q.add(new Node(i, j, map[i][j]));
-				test[i][j] = ++index;
-				visit[i][j] = true;
-				room++;
-				size = 1;
-
-				while(!q.isEmpty()) {
-					node = q.poll();
+				for(int j=0;j<4;j++) {
+					nr = node.r + dr[j];
+					nc = node.c + dc[j];
 					
-
-					for(int d=0;d<4;d++) {
-						
-						if((node.d & ( 1 << d)) != 0) continue;
-						
-						nr = node.r + dr[d];
-						nc = node.c + dc[d];
-						
-						if(inArea(nr,nc) && !visit[nr][nc]) {
-							test[nr][nc] = index;
-							visit[nr][nc] = true;
-							q.add(new Node(nr, nc, map[nr][nc]));
-							size++;
+					if(inArea(nr, nc) && !visit[nr][nc]) {
+						if(map[nr][nc] == 0) {
+							map[nr][nc] = node.num;
+							virus.add(new Node(nr, nc, map[nr][nc]));
+							zero--;
 						}
+						else {
+							if(map[nr][nc] != node.num) map[nr][nc] = 3;
+						}
+						
 					}
 				}
-				dp.add(size);
-				max = Math.max(max, size);
 			}
+			if(zero == 0) break;
 		}
-		
-		visit = new boolean[room+1][room+1];
-		int doubleRoom = 1;
-		
-		for(int i=0;i<N;i++) {
-			for(int j=0;j<M;j++) {
-				
-				size = test[i][j];
-				for(int k=0;k<4;k++) {
-					nr = i + dr[k];
-					nc = j + dc[k];
-					
-					if(!inArea(nr,nc)) continue;
-					if(size == test[nr][nc]) continue;
-					if(visit[size][test[nr][nc]]) continue;
-//					System.out.println("[start,end]: ["+size+","+test[nr][nc]+"]");
-					visit[size][test[nr][nc]] = visit[test[nr][nc]][size] = true;
-					doubleRoom = Math.max(dp.get(size-1)+dp.get(test[nr][nc]-1), doubleRoom);
-					
-//					if(inArea(nr,nc) && !visit[nr][nc] && size != test[nr][nc]) {
-//						doubleRoom = Math.max(dp.get(size-1)+dp.get(test[i][j]-1), doubleRoom);
-//						size = test[i][j];
-//						visit[size][test[i][j]] = visit[test[i][j]][size] = true;
-//					}
-				}
-				
-//				if(size != test[i][j] && !visit[size][test[i][j]]) {
-//					doubleRoom = Math.max(dp.get(size-1)+dp.get(test[i][j]-1), doubleRoom);
-//					size = test[i][j];
-//					visit[size][test[i][j]] = visit[test[i][j]][size] = true; 
-//				}
-				
-			}
-		}
-		
-//		for(int i=0;i<N;i++)System.out.println(Arrays.toString(map[i]));
-		
-//		for(int i=0;i<N;i++)System.out.println(Arrays.toString(test[i]));
-//		System.out.println(dp);
-		StringBuilder sb = new StringBuilder();
-		sb.append(room).append("\n").append(max).append("\n").append(doubleRoom);
-		System.out.println(sb);
-		
-		
-		
 	}
 
 	private static boolean inArea(int nr, int nc) {
 		return nr >= 0 && nr < N && nc >= 0 && nc < M;
 	}
 }
+
+
+
+
+
+
+
 
 
 
