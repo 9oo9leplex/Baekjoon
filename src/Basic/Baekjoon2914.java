65d67a2820c3153a9547149a10ccdb08caafbb5b
연창모 <9oo9leplex@gmail.com>
2022-05-24 16:55:07 +0900

    BASIC,Baekjoon2914

--- src/Basic/Baekjoon2914.java
+++ src/Basic/Baekjoon2914.java
@@ -5,10 +5,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {		
-	
+public class Baekjoon2914 {
 
-	public static void main(String[] args) throws IOException{
+public static void main(String[] args) throws IOException{
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
