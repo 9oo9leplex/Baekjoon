feef11181aeea91a55e0f26d1ff058149dfd520b
연창모 <9oo9leplex@gmail.com>
2022-05-26 22:45:22 +0900

    BASIC,Baekjoon2845

--- src/Basic/Baekjoon2845.java
+++ src/Basic/Baekjoon2845.java
@@ -5,27 +5,22 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {		
-	
+public class Baekjoon2845 {
 
-	public static void main(String[] args) throws IOException{
-		
+	public static void main(String[] args) throws IOException {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
+
 		int l = Integer.parseInt(st.nextToken());
 		int p = Integer.parseInt(st.nextToken());
-		int lp = l * p ;
+		int lp = l * p;
 		st = new StringTokenizer(br.readLine());
 		StringBuilder sb = new StringBuilder();
-		for(int i=0;i<5;i++) {
+		for (int i = 0; i < 5; i++) {
 			sb.append(Integer.parseInt(st.nextToken()) - lp).append(" ");
 		}
-		
+
 		System.out.println(sb);
-		
-
-	
-
 	}
 }
--- src/Graph/Main.java
+++ src/Graph/Main.java
@@ -15,45 +15,21 @@ public class Main {
 
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
-
-		int map[][] = new int[n][n];
-		int max = n * (n * (n - 1) / 2);
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++)
-				map[i][j] = max;
-		}
-
-		int first, second;
-
-		for (int i = 0; i < m; i++) {
-			st = new StringTokenizer(br.readLine());
-
-			first = Integer.parseInt(st.nextToken()) - 1;
-			second = Integer.parseInt(st.nextToken()) - 1;
-			map[first][second] = 1;
-		}
-
-		for (int k = 0; k < n; k++) {
-			for (int i = 0; i < n; i++) {
-				for (int j = 0; j < n; j++) {
-					if (map[i][j] > map[i][k] + map[k][j])
-						map[i][j] = map[i][k] + map[k][j];
-				}
+		
+		int cnt = 1;
+		int block = Integer.parseInt(br.readLine());
+//		int com = Integer.parseInt(br.readLine());
+		int tmp;
+		for(int i=0;i<n - 1;i++) {
+			tmp = Integer.parseInt(br.readLine());
+			if(block == tmp) cnt++;
+			else if(block < tmp) {
+				block = tmp;
+				cnt = 1;
 			}
+			System.out.println("[cnt,block]: ["+cnt+","+block+"]");
 		}
-
-		int result = 0;
-		for (int i = 0; i < n; i++) {
-			int cnt = 0;
-			for (int j = 0; j < n; j++) {
-				if (map[i][j] < max || map[j][i] < max)
-					cnt++;
-			}
-			if (cnt == n - 1)
-				result++;
-		}
-
-		System.out.println(result);
+		System.out.println(cnt+" "+block);
 	}
 
 }
