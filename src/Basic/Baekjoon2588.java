7a91223a817907a0540c6eb3425f3ec75ff5d467
연창모 <9oo9leplex@gmail.com>
2022-07-29 00:00:42 +0900

    Basic,Baekjoon,B4,Baekjoon2588

---
+++ src/Basic/Baekjoon2588.java
@@ -0,0 +1,28 @@
+package Basic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon2588 {
+
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int a = Integer.parseInt(br.readLine());
+		char[] b = br.readLine().toCharArray();
+		
+		StringBuilder sb = new StringBuilder();
+		int sum = 0;
+		int tmp,idx = 0;
+		for(int i=2;i>=0;i--) {
+			tmp = a * (b[i] - '0') ;
+			sb.append(tmp).append("\n");
+			sum += tmp * (int)Math.pow(10, idx++);
+		}
+		sb.append(sum);
+		System.out.println(sb);
+		
+}
+}
--- src/Basic/Main.java
+++ src/Basic/Main.java
@@ -3,8 +3,6 @@ package Basic;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {		
 	
@@ -12,14 +10,19 @@ public class Main {
 			
 			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	
-			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(br.readLine());
+			char[] b = br.readLine().toCharArray();
 			
-			int[] arr = new int[4];
-			for(int i=0;i<4;i++) arr[i] = Integer.parseInt(st.nextToken());
-			
-			Arrays.sort(arr);
-			
-			System.out.println(Math.abs(arr[3] + arr[0] - arr[2] - arr[1]));
+			StringBuilder sb = new StringBuilder();
+			int sum = 0;
+			int tmp,idx = 0;
+			for(int i=2;i>=0;i--) {
+				tmp = a * (b[i] - '0') ;
+				sb.append(tmp).append("\n");
+				sum += tmp * (int)Math.pow(10, idx++);
+			}
+			sb.append(sum);
+			System.out.println(sb);
 			
 	}
 }
