39244bcde18473a41f5a557afb6485e125e66ffc
연창모 <9oo9leplex@gmail.com>
2022-07-30 09:21:31 +0900

    Basic,Baekjoon,B4,
    Baekjoon14924

--- src/Basic/Main.java
+++ src/Basic/Main.java
@@ -3,6 +3,7 @@ package Basic;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {		
 	
@@ -10,19 +11,13 @@ public class Main {
 			
 			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	
-			int a = Integer.parseInt(br.readLine());
-			char[] b = br.readLine().toCharArray();
+			StringTokenizer st = new StringTokenizer(br.readLine());
 			
-			StringBuilder sb = new StringBuilder();
-			int sum = 0;
-			int tmp,idx = 0;
-			for(int i=2;i>=0;i--) {
-				tmp = a * (b[i] - '0') ;
-				sb.append(tmp).append("\n");
-				sum += tmp * (int)Math.pow(10, idx++);
-			}
-			sb.append(sum);
-			System.out.println(sb);
+			int s = Integer.parseInt(st.nextToken());
+			int t = Integer.parseInt(st.nextToken());
+			int d = Integer.parseInt(st.nextToken());
+			
+			System.out.println(d / (s*2) * t);
 			
 	}
 }
