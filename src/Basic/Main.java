feac463d3d1c3d6b272ebb9463a4045b4c5734d3
연창모 <9oo9leplex@gmail.com>
2022-08-04 22:26:31 +0900

    Basic,Baekjoon,B3,Baekjoon10818

--- src/Basic/Main.java
+++ src/Basic/Main.java
@@ -3,6 +3,7 @@ package Basic;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {		
@@ -11,18 +12,16 @@ public class Main {
 			
 			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	
+			int n = Integer.parseInt(br.readLine());
+			int arr[] = new int[n];
+			
 			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int i=0;i<n;i++) arr[i] = Integer.parseInt(st.nextToken());
 			
-			int h = Integer.parseInt(st.nextToken());
-			int m = Integer.parseInt(st.nextToken());
+			Arrays.sort(arr);
 			
-			if(m < 45) {
-				h--;
-				m += 15;
-			} else m -= 45;
-			if(h < 0) h = 23;
+			System.out.println(arr[0] + " " + arr[n-1]);
 			
-			System.out.println(h+" "+m);
 	}
 }
 
