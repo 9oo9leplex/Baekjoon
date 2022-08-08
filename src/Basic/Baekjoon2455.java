2ac7b6839f55af0bf00a73e8138e3d4a0b4e1b71
연창모 <9oo9leplex@gmail.com>
2022-08-08 23:05:22 +0900

    Basic,Baekjoon,B3,Baekjoon2455

---
+++ src/Basic/Baekjoon2455.java
@@ -0,0 +1,32 @@
+package Basic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baekjoon2455 {
+
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st;
+		
+		int in,out,sum = 0,max = 0;
+		
+		for(int i=0;i<4;i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			out = Integer.parseInt(st.nextToken());
+			in = Integer.parseInt(st.nextToken());
+			
+			sum -= out;
+			sum += in;
+			max = Math.max(max, sum);
+			
+		}
+		System.out.println(max);
+		
+}
+}
--- src/Basic/Main.java
+++ src/Basic/Main.java
@@ -3,7 +3,6 @@ package Basic;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {		
@@ -13,20 +12,22 @@ public class Main {
 			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	
 			StringTokenizer st;
-			int arr[] = new int[3];
-			StringBuilder sb = new StringBuilder();
 			
-			while(true) {
+			int in,out,sum = 0,max = 0;
+			
+			for(int i=0;i<4;i++) {
 				st = new StringTokenizer(br.readLine());
 				
-				for(int i=0;i<3;i++) arr[i] = Integer.parseInt(st.nextToken());
-				if(arr[0] == 0 && arr[1] == 0 && arr[2] == 0) break;
+				out = Integer.parseInt(st.nextToken());
+				in = Integer.parseInt(st.nextToken());
 				
-				Arrays.sort(arr);
+				sum -= out;
+				sum += in;
+				max = Math.max(max, sum);
 				
-				sb.append((int)Math.pow(arr[0], 2) + (int)Math.pow(arr[1], 2) == (int)Math.pow(arr[2], 2) ? "right" : "wrong").append("\n");
 			}
-			System.out.println(sb);
+			System.out.println(max);
+			
 	}
 }
 
