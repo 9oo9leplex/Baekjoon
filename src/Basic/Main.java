2e9b13e3878d4d1a048693ec179660c0f618621d
연창모 <9oo9leplex@gmail.com>
2022-08-24 23:51:38 +0900

    Basic,Baekjoon,B3,Baekjoon15894

--- src/Basic/Main.java
+++ src/Basic/Main.java
@@ -3,7 +3,6 @@ package Basic;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {		
 	
@@ -11,17 +10,9 @@ public class Main {
 			
 			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	
-			StringTokenizer st;
-			int result = 0,max = 0;
+			long n = Integer.parseInt(br.readLine());
 			
-			for(int i=0;i<10;i++) {
-				st = new StringTokenizer(br.readLine());
-				result -= Integer.parseInt(st.nextToken());
-				result += Integer.parseInt(st.nextToken());
-				
-				max = Math.max(max, result);
-			}
-			System.out.println(max);
+			System.out.println(n*4);
 			
 			
 	}
