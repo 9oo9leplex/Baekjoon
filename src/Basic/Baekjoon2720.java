90d6c6b0e7aa41d2edf55f7673f7b4b63841fc05
연창모 <9oo9leplex@gmail.com>
2022-09-02 22:47:06 +0900

    Basic,Baekjoon,B3,Baekjoon2720

--- src/Basic/Baekjoon2720.java
+++ src/Basic/Baekjoon2720.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class Baekjoon2720 {
 
 	public static void main(String[] args) throws IOException {
 
