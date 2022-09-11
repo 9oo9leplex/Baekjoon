c8ae72b06e96f98dedc039375615979763f4e7ad
연창모 <9oo9leplex@gmail.com>
2022-09-11 18:17:06 +0900

    Basic,Baekjoon,B3,Baekjoon17496

--- src/Basic/Baekjoon17496.java
+++ src/Basic/Baekjoon17496.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Baekjoon17496 {
 
 	public static void main(String[] args) throws IOException {
 
