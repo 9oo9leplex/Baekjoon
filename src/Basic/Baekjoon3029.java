f885a3b1d9dce6affa47dfa4f5d68a6517aad184
연창모 <9oo9leplex@gmail.com>
2022-09-26 23:31:42 +0900

    BASIC,Baekjoon,B3,Baekjoon3029

--- src/Basic/Baekjoon3029.java
+++ src/Basic/Baekjoon3029.java
@@ -8,44 +8,33 @@ import java.util.StringTokenizer;
 public class Baekjoon3029 {
 
 	public static void main(String[] args) throws IOException {
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer st = new StringTokenizer(br.readLine(),":");
-		
-		int sh = Integer.parseInt(st.nextToken());
-		int sm = Integer.parseInt(st.nextToken());
-		int ss = Integer.parseInt(st.nextToken());
-		
-		st = new StringTokenizer(br.readLine(),":");
-		int eh = Integer.parseInt(st.nextToken());
-		int em = Integer.parseInt(st.nextToken());
-		int es = Integer.parseInt(st.nextToken());
-		
-		if(es < ss) {
-			es += 60;
-			em--;
+		String S = br.readLine();
+		String S2 = br.readLine();
+		String[] s = S.split(":");
+		String[] s2 = S2.split(":");
+		int[] N = new int[3];
+		//int형으로 형변환 하면서 시간의 차를 시 분 초 기준으로 저장한다.
+		for(int i = 0; i < 3; i++) {
+			N[i] = Integer.parseInt(s2[i]) - Integer.parseInt(s[i]);
 		}
-		es -= ss;
 		
-		if(em < sm) {
-			em += 60;
-			eh--;
+		if(S.equals(S2)) {
+			System.out.println("24:00:00");
+		}else {
+			if(N[2] < 0) {
+				N[2] += 60;
+				N[1]--;
+			}
+			if(N[1] < 0) {
+				N[1] += 60;
+				N[0]--;
+			}
+			if(N[0] < 0) {
+				N[0] += 24;
+			}
+			//00:00:00방식으로 출력하기 위한 포맷
+			System.out.format("%02d:%02d:%02d", N[0], N[1], N[2]);
 		}
-		em -= sm;
-		if(sh > eh) eh += 24;
-		eh -= sh;
-		String result= "";
-		if(eh < 10) result += "0";
-		result += eh+":";
-		if(em < 10) result += "0";
-		result += em+":";
-		if(es < 10) result += "0";
-		result += es;
-		System.out.println(result);
-
-		
-
 	}
-
 }
