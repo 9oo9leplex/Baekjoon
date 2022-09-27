97352eea38c39804fe0720f5fd4e760e635f1c70
연창모 <9oo9leplex@gmail.com>
2022-09-27 23:24:20 +0900

    BASIC,Baekjoon,B3,Baekjoon16431

--- src/Basic/Baekjoon16431.java
+++ src/Basic/Baekjoon16431.java
@@ -3,9 +3,9 @@ package Basic;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class Main {
-	
-	public static void main(String[] args) throws IOException {
+public class Baekjoon16431 {
+
+public static void main(String[] args) throws IOException {
 		
 		Scanner scan = new Scanner(System.in);
     	
