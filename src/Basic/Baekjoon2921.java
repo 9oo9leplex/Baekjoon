b953183b53cf1f9f3552722a2f3eb58a3f29d7b8
연창모 <9oo9leplex@gmail.com>
2022-09-28 23:14:23 +0900

    BASIC,Baekjoon,B3,Baekjoon2921

--- src/Basic/Baekjoon2921.java
+++ src/Basic/Baekjoon2921.java
@@ -4,9 +4,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class Baekjoon2921 {
 
-    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) throws IOException {
         int n = Integer.parseInt(br.readLine());
