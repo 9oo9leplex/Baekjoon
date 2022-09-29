ef27c3079e30b10c3006a8d2ff89be8d3e6abdb2
연창모 <9oo9leplex@gmail.com>
2022-09-29 23:10:32 +0900

    BASIC,Baekjoon,B3,Baekjoon4299

--- src/Basic/Baekjoon4299.java
+++ src/Basic/Baekjoon4299.java
@@ -5,9 +5,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Baekjoon4299 {
 
-    public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
 
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     	StringTokenizer st = new StringTokenizer(br.readLine());
