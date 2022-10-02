9e58f4d8bf504a59e0d48fac10509fca19899cbb
연창모 <9oo9leplex@gmail.com>
2022-10-02 23:12:07 +0900

    BASIC,Baekjoon,B3,Baekjoon2959

--- src/Basic/Baekjoon2959.java
+++ src/Basic/Baekjoon2959.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Main {
+public class Baekjoon2959 {
 
 	public static void main(String[] args) throws IOException {
 
