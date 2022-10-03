080512b9dcd9e7b833d0ab4fa5da96046b8b518c
연창모 <9oo9leplex@gmail.com>
2022-10-03 23:40:16 +0900

    BASIC,Baekjoon,B3,Baekjoon11966

--- src/Basic/Baekjoon11966.java
+++ src/Basic/Baekjoon11966.java
@@ -2,8 +2,9 @@ package Basic;
 
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args){
+public class Baekjoon11966 {
+
+	public static void main(String[] args){
         Scanner sc = new Scanner(System.in);                                                            
         int num = sc.nextInt();
  
